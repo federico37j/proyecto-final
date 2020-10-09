c5b93574e18cc6f54a410a8b7e394c0fbce2142f
Federico <federicoarambillet37@gmail.com>
2020-10-09 17:24:14 -0300

    Se termina de implementar el carrusel de imágenes.

--- client/js/carrusel.js
+++ client/js/carrusel.js
@@ -1,22 +1,28 @@
 "use strict";
 
-
+//Se crea un arreglo con la ruta de cada imagen.
 let imagenes = new Array(
     'img/banner.jpg',
     'img/banner1.jpg',
-    'img/banner2.jpg'
+    'img/banner2.jpg',
+    'img/banner3.jpg'
 );
 
+//Se obtiene un número aleatorio entre 0 y la cantidad de imágenes en el arreglo.
 function obtenerIndexAleatorio() {
     return Math.floor(Math.random() * imagenes.length);
 }
 
+//Dependiendo del número devuelto por la función obtenerIndexAleatorio() es el índex de la imagen que se va a mostrar.
 function mostrarBannerAleatorio() {
     let index =  obtenerIndexAleatorio();
     document.querySelector('#img-banner').src = imagenes[index];
 }
 
-onload = function () {
+//llama a la función mostrarBannerAleatorio() cada 4s.
+function cargarBanner() {
     mostrarBannerAleatorio();
     setInterval(mostrarBannerAleatorio, 4000);
 }
+
+cargarBanner();
