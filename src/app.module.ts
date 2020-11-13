86eb07d7e31bd76ba60ac79ed636312da90e3ebb
nico-casas <nicolas.f.casas@gmail.com>
2020-11-13 19:31:48 -0300

    back carrito

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,15 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { NegocioController } from './negocio/negocio.controller';
 import { NegocioService } from './negocio/negocio.service';
+import { CarritoController } from './carrito/carrito.controller';
+import { CarritoService } from './carrito/carrito.service';
 
 @Module({
   imports: [ServeStaticModule.forRoot({
     rootPath: join(__dirname, '..',
     'client'),
     }),],
-  controllers: [AppController, NegocioController],
-  providers: [AppService, NegocioService],
+  controllers: [AppController, NegocioController, CarritoController],
+  providers: [AppService, NegocioService, CarritoService],
 })
 export class AppModule {}
