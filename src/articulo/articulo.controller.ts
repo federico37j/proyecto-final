0ea9fe5f4bc136ba25b1ab6e0b5826031af36fd9
Federico <federicoarambillet37@gmail.com>
2020-12-06 20:57:23 -0300

    Se implementa control del stock.

--- src/articulo/articulo.controller.ts
+++ src/articulo/articulo.controller.ts
@@ -1,25 +1,60 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { ArticuloService } from './articulo.service';
 import { Articulo } from './articulo.entity';
+import { Imagen_articulo } from 'src/imagen-articulo/imagen-articulo.entity';
 
 @Controller('articulo')
 export class ArticuloController {
 
     public constructor(private readonly articuloService: ArticuloService) { }
 
-    @Get("get-all")
+    @Get('get-all')
     public getAllProductos(): Promise<Articulo[]> {
         return this.articuloService.getArticulos();
     }
 
-    @Get(':categoria/:index')
-    public getArticuloById(@Param('categoria') categoria, @Param('index') index): Promise<Articulo> {
-        return this.articuloService.getArticuloById(categoria, Number(index));
+    @Get(':categoria/:id')
+    public getArticuloById(@Param('categoria') categoria, @Param('id') id): Promise<Articulo> {
+        return this.articuloService.getArticuloById(Number(id));
     }
+
+    // @Get(':index')
+    // public getImgByIdArticulo(@Param('index') index): Promise<Imagen_articulo[]> {
+    //     return this.articuloService.getImagenesByIdArticulo(Number(index));
+    // }
 
     @Get(':categoria')
     public getArticulosCategoria(@Param('categoria') categoria): Promise<Articulo[]> {
         return this.articuloService.getArticulosCategoria(categoria);
     }
 
+    @Post(':categoria')
+    public addArticulo(@Body() articulo: Articulo, @Param('categoria') categoria): Promise<Articulo> {
+        return this.articuloService.addArticulo(articulo, categoria);
+    }
+
+     // @Post('addImagen')
+    // public addImagen(@Body() img: Imagen_articulo): Promise<Imagen_articulo> {
+    //     return this.articuloService.addImagen(img);
+    // }
+
+    @Put(':categoria/:id')
+    public updateArticulo(@Body() art: any, @Param('categoria') categoria, @Param('id') index): Promise<Articulo> {
+        return this.articuloService.updateArticulo(art, categoria, Number(index));
+    }
+
+    // @Put(':id')
+    // public updateImagen(@Body() art: any, @Param('id') index) {
+    //     return this.articuloService.updateImagen(art, Number(index));
+    // }
+
+    @Delete(':id')
+    public deleteArticulo(@Param ('id') id) {
+        return this.articuloService.deleteArticulo(id);
+    }
+
+    // @Delete(':id')
+    // public deleteArticulo(@Param ('id') id) {
+    //     return this.articuloService.deleteImagenes(id);
+    // }
 }
--- src/articulo/articulo.service.ts
+++ src/articulo/articulo.service.ts
@@ -17,6 +17,59 @@ export class ArticuloService {
         // this.listadoArticulos = this.getArticulos();
     }
 
+    //Creo un artículo con los datos que vienen por parámetro.
+    public async addArticulo(newArticulo: any, categoria: string): Promise<Articulo> {
+        try {
+            const articuloCreado: Articulo = await this.articuloRepository.save(new Articulo(
+                newArticulo.nombre,
+                Number(newArticulo.precio),
+                newArticulo.financiacion,
+                newArticulo.detalle,
+                newArticulo.tipo,
+                Number(newArticulo.stock),
+                this.categoriaStrigAid(categoria)
+            )
+            );
+            if (articuloCreado) {
+                //Si el artículo se creó, hago lo mismo con las imágenes.
+                this.addImagen(newArticulo.imagen_articulo, articuloCreado.getNroArticulo())
+                return articuloCreado;
+            } else {
+                throw new HttpException('No se pudo crear un articulo', HttpStatus.NOT_FOUND);
+            }
+        } catch (error) {
+            throw new HttpException({
+                status: HttpStatus.NOT_FOUND,
+                error: "there is an error in the request, " + error,
+            }, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    public async addImagen(newImagenes: any[], nroArticulo: number) {
+        try {
+            //let arrImagenes: Promise<Imagen_articulo[]>;
+            for (let i = 0; i < newImagenes.length; i++) {
+                let principal;
+                if (i == 0) {
+                    principal = true;
+                } else {
+                    principal = false;
+                }
+                const imgCreada: Imagen_articulo = await this.imagenArticuloRepository.save(new Imagen_articulo(
+                    newImagenes[i],
+                    principal,
+                    nroArticulo
+                ));
+                // (await arrImagenes).push(imgCreada);
+            }
+        } catch (error) {
+            throw new HttpException({
+                status: HttpStatus.NOT_FOUND,
+                error: "there is an error in the request, " + error,
+            }, HttpStatus.NOT_FOUND);
+        }
+    }
+
     //Devuelve lista completa de artículos con sus respectivas imágenes.
     public async getArticulos(): Promise<Articulo[]> {
         try {
@@ -98,15 +151,118 @@ export class ArticuloService {
         return this.getArticulosPorCategoria(this.categoriaStrigAid(categoria));
     }
 
-    // Devuelvo un artículo según el índex.
-    public async getArticuloById(categoria: string, index: number): Promise<Articulo> {
+    // Devuelvo un artículo según el id.
+    public async getArticuloById(id: number): Promise<Articulo> {
         let response: Articulo = await this.articuloRepository.findOne({
             relations: ["imagen_articulo"],
             where: [{
-                "idArticulo": Equal(index)
+                "idArticulo": Equal(id)
             }]
         })
-        return response
+        return response;
     }
 
+    // Devuelvo las imagenes pertenecientes a ese id_articulo
+    public async getImagenesByIdArticulo(id: number): Promise<Imagen_articulo[]> {
+        let response: Imagen_articulo[] = await this.imagenArticuloRepository.find({
+            where: [{
+                "idArticulo": Equal(id)
+            }]
+        })
+        return response;
+    }
+
+    //Me traigo desde la bd el artículo con ese id, si existe con los sets lo modifico.
+    public async updateArticulo(updateArticulo: any, categoria: string, id: number): Promise<Articulo> {
+        let articulo: Articulo = await this.getArticuloById(id);
+        try {
+            if (articulo) {
+                articulo.setNombre(updateArticulo.nombre);
+                articulo.setPrecio(updateArticulo.precio);
+                articulo.setFinanciacion(updateArticulo.financiacion);
+                articulo.setDetalle(updateArticulo.detalle);
+                articulo.setTipo(updateArticulo.tipo);
+                articulo.setStock(updateArticulo.stock);
+                const articuloUpdated: Articulo = await this.articuloRepository.save(articulo);
+
+                if (articuloUpdated) {
+                    //Si el artículo se actualizo, hago lo mismo con las imágenes.
+                    this.updateImagen(updateArticulo.imagenes, articulo.getNroArticulo());
+                    return articulo;
+                } else {
+                    throw new HttpException('No se pudo crear el artículo', HttpStatus.NOT_MODIFIED);
+                }
+            } else {
+                throw new HttpException('No se pudo crear el artículo', HttpStatus.NOT_FOUND);
+            }
+        } catch (error) {
+            throw new HttpException({
+                status: HttpStatus.NOT_FOUND,
+                error: "there is an error in the request, " + error,
+            }, HttpStatus.NOT_FOUND);
+        }
+    }
+
+
+    //Actualizo las imágenes actuales y me fijo si hay imágenes nuevas para cargar en el caso que tenga llamo a la función addImagen.
+    public async updateImagen(newImagenes: any[], nroArticulo: number) {
+        let imagenes: Imagen_articulo[] = await this.getImagenesByIdArticulo(nroArticulo);
+        try {
+            if (imagenes) {
+                for (let i = 0; i < imagenes.length; i++) {
+                    imagenes[i].setImagen(newImagenes[i]);
+                    const imagenesUpdated: Imagen_articulo = await this.imagenArticuloRepository.save(imagenes[i]);
+                    if (imagenesUpdated) {
+                    }
+                }
+                if (newImagenes.length !== imagenes.length) {
+                    let imgNuevas: string[] = []
+                    for (let i = imagenes.length; i < newImagenes.length; i++) {
+                        imgNuevas.push(newImagenes[i])
+                    }
+                    this.addImagen(imgNuevas, nroArticulo);
+                }
+            }
+        } catch (error) {
+            throw new HttpException({
+                status: HttpStatus.NOT_FOUND,
+                error: "there is an error in the request, " + error,
+            }, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    //Elimino todas las imagenes que esten relacionadas con ese id de artículo.
+    public async deleteImagenes(id: number) {
+        try {
+            await this.imagenArticuloRepository.query(`DELETE FROM IMAGEN_ARTICULO WHERE idArticulo = ${id}`);
+        } catch (error) {
+            throw new HttpException({
+                status: HttpStatus.NOT_FOUND,
+                error: "there is an error in the request, " + error,
+            }, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    //Me traigo desde la bd el artículo con ese id, si existe elimino primero las imagenes que tiene referenciadas y despues elimino el artículo.
+    public async deleteArticulo(id: number): Promise<boolean> {
+        try {
+            let articulo = await this.getArticuloById(id);
+            if (articulo) {
+                await this.deleteImagenes(id);
+                let deletResult = await this.articuloRepository.delete(id);
+                if (deletResult.affected) {
+                    console.log("Elemento eliminado");
+                    return true;
+                }
+            } else {
+                throw new HttpException('No se pudo eliminar el artículo', HttpStatus.NOT_MODIFIED);
+            }
+        } catch (error) {
+            throw new HttpException({
+                status: HttpStatus.NOT_FOUND,
+                error: "there is an error in the request, " + error,
+            }, HttpStatus.NOT_FOUND);
+        }
+
+    }
 }
