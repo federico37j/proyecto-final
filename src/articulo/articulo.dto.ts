f186e6e4cbaaf641f16b8124d9aeb843f73e6f46
Federico <federicoarambillet37@gmail.com>
2020-12-06 21:16:13 -0300

    Se crean los Data Transfer Object de articulo e imagen_articulo

---
+++ src/articulo/articulo.dto.ts
@@ -0,0 +1,8 @@
+export class ArticuloDTO {
+    readonly nombre: string;
+    readonly precio: number;
+    readonly financiacion: string;
+    readonly detalle: string;
+    readonly tipo: string;
+    readonly stock: number;
+}
